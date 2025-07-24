68d16d4e6d069d754953f22f8a2425693299d498
Kaan Menevşe <105436254+redempkaan@users.noreply.github.com>
2025-07-24 22:25:43 +0300

    Delete LoginAPI directory

--- LoginAPI/services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using LoginAPI.Models;
-using LoginAPI.Models.DTOs;
-
-public interface IAuthService
-{
-    Task<bool> UserExists(string username);
-    Task<User> Register(RegisterDto dto);
-    Task<string> Login(LoginDto dto);
-}
